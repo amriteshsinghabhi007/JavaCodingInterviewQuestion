5d0ba7ec23182b760d772f53240289f0323279c1
amriteshsinghabhi007 <amriteshsinghabhi526@gmail.com>
2024-10-21 22:12:04 +0530

    day - 6 - Patterns

--- src/Patterns/InvertedHalfPyramid180Degree.java
+++ src/Patterns/InvertedHalfPyramid180Degree.java
@@ -1,8 +1,14 @@
 package Patterns;
 
+//     *
+//    **
+//   ***
+//  ****
+// *****
+//******
 public class InvertedHalfPyramid180Degree {
     public static void main(String[] args) {
-        int row = 6;
+        int row = 10;
         for (int i = 1; i<=row; i++){
             for (int j = 1; j<=row-i; j++){
                 System.out.print(" ");
